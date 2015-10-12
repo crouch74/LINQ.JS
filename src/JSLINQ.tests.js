cd6c5b996322114a228888ce580e761bf0bc736f
crouch74 <ahmedaeid@outlook.com>
2015-10-12 22:55:52 +0200

    add some tests

--- src/JSLINQ.tests.js
+++ src/JSLINQ.tests.js
@@ -1,3 +1,13 @@
 /// <reference path="JSLINQ.js">
 
 "use strict";
+describe("To LINQ",function(){
+  it("Is Defined",function(){
+    var x = [1,2,3];
+    expect(x.toLINQ).toBeDefined();
+  });
+  it("Returns LINQ",function(){
+    var x = [1,2,3];
+    expect(x.toLINQ()).toEqual(new LINQ(x));
+  });
+});
