548025ff23c535db103443a5ff9d55ab2374b05c
crouch74 <ahmedaeid@outlook.com>
2015-10-18 02:17:40 +0200

    add documentation add linqer.min.js add src/linqer.js

---
+++ DOCUMENTATION.md
@@ -0,0 +1,352 @@
+NOTES
+=====
+
+-	This library implements the concept of deferred evaluation just as .NET LINQ does ,, so functions like : select, selectMany , where , etc. will not be evaluated until you use toArray or toDictionary functions .. otherwise functions like : sum , average , max , min , etc. will be executed immediately.
+
+-	This library supports chaining like this
+
+```
+var points = [{"x":0,"y":0},{"x":0,"y":1},{"x":1,"y":0},{"x":1,"y":1}];
+new LINQ(points)
+      .select("p=>p.x")
+      .where("p=>p.x>=1")
+      .toArray()
+```
+
+-	This library supports C#-Lambda expression as literals or function and of course if you support ES6 it can support JS arrow functions as follows
+
+```
+new LINQ([1,2,3,4]).where("n=>n>2");
+new LINQ([1,2,3,4]).where(function(n){return n>2});
+//if you support ES6
+new LINQ([1,2,3,4]).where(n=>n>2);
+// all of the above is the same
+```
+
+-	This library supports one level lambda expressions and without parentheses around the parameters
+
+```
+s=>s.age //accepted
+s,t=>{"studentName":s.name , "teacherName":t.name} //accepted
+(s)=>s.age //not accepted
+(s,t)=>{"studentName":s.name , "teacherName":t.name} //not accepted
+petOwner=>petOwner.pets.select(pet=>pet.name) //not accepted
+```
+
+select
+======
+
+This function maps a list of objects to a new list of objects based on a function you provide that accepts an object as a parameter and returns an object
+
+```
+var points = [{"x":0,"y":0},{"x":0,"y":1},{"x":1,"y":0},{"x":1,"y":1}];
+new LINQ(points).select("p=>p.x").toArray() // [0,0,1,1]
+```
+
+where
+=====
+
+This function is used to filter an array based on a specific criteria provided as a function that accepts an object as a param and returns a boolean
+
+```
+var points = [{"x":0,"y":0},{"x":0,"y":1},{"x":1,"y":0},{"x":1,"y":1}];
+new LINQ(points).where("p=>p.x==p.y").toArray() // [{"x":0,"y":0},{"x":1,"y":1}]
+```
+
+count
+=====
+
+This function evaluates all the queries and returns the count of the result
+
+```
+var points = [{"x":0,"y":0},{"x":0,"y":1},{"x":1,"y":0},{"x":1,"y":1}];
+new LINQ(points).where("p=>p.x==p.y").count(); // 2
+new LINQ(points).count("p=>p.x==p.y"); // 2
+```
+
+any
+===
+
+This function evaluates all the queries and returns true if there is at least one object in the array that matches the provided criteria
+
+```
+var points = [{"x":0,"y":0},{"x":0,"y":1},{"x":1,"y":0},{"x":1,"y":1}];
+new LINQ(points).any("p=>p.x==p.y"); // true
+new LINQ(points).any("p=>p.x>10"); // false
+```
+
+all
+===
+
+This function evaluates all the queries and returns true only if all the objects inside an array meets a specific criteria
+
+```
+var points = [{"x":0,"y":0},{"x":0,"y":1},{"x":1,"y":0},{"x":1,"y":1}];
+new LINQ(points).all("p=>p.x==p.y"); // false
+new LINQ(points).all("p=>p.x<10"); // true
+```
+
+sum
+===
+
+This function evaluates all the queries and returns the sum of all the array or you can pass a selector function as a parameter to it
+
+```
+var numbers = [1,2,3,4,5,6,7,8,9,10];
+new LINQ(numbers)
+      .where("x=>x%2==0")
+      .sum(); //30
+var classes = [{"class":"1",numberOfStudens:20},{"class":"2",numberOfStudens:15},{"class":"3",numberOfStudens:30},{"class":"4",numberOfStudens:50}]
+new LINQ(classes)
+  .sum("c=>c.numberOfStudens"); //115
+```
+
+average
+=======
+
+This function evaluates all the queries and returns the average of all the array or you can pass a selector function as a parameter to it
+
+```
+var classes = [{"class":"1",numberOfStudens:20},{"class":"2",numberOfStudens:15},{"class":"3",numberOfStudens:30},{"class":"4",numberOfStudens:50}]
+new LINQ(classes)
+  .average("c=>c.numberOfStudens"); //28.75
+```
+
+skip
+====
+
+Bypasses a specified number of elements in a sequence and then returns the remaining elements. [MSDN](https://msdn.microsoft.com/en-us/library/vstudio/bb358985.aspx)
+
+```
+new LINQ([1,2,3,4,5,6,7]).skip(3).toArray(); //[4,5,6,7]
+```
+
+take
+====
+
+Returns a specified number of contiguous elements from the start of a sequence. [MSDN](https://msdn.microsoft.com/en-us/library/vstudio/bb503062.aspx)
+
+```
+new LINQ([1,2,3,4,5,6,7]).take(4).toArray(); //[1,2,3,4]
+```
+
+max
+===
+
+Returns the maximum value in the array .
+
+This function can accept a selector function
+
+```
+new LINQ([1,2,3,4,5,6]).max(); //6
+```
+
+min
+===
+
+Returns the minimum value in the array .
+
+This function can accept a selector function
+
+```
+new LINQ([1,2,3,4,5,6]).min(); //1
+```
+
+reverse
+=======
+
+Inverts the order of the elements in a sequence. [MSDN](https://msdn.microsoft.com/en-us/library/vstudio/bb358497.aspx)
+
+```
+new LINQ([1,2,3,4,5]).reverse().toArray(); //[5,4,3,2,1]
+```
+
+removeAll
+=========
+
+This function removes all elements that matches a specific criteria provided as a function in the first parameter
+
+```
+
+new LINQ([1,2,3,4,5,6,7]).removeAll("x=>x<5").toArray(); //[5,6,7]
+
+```
+
+single / singleOrDefault
+========================
+
+```
+new LINQ([1]).single(); //1
+new LINQ([1,2]).single(); //exception
+new LINQ([]).single(); //exception
+new LINQ([1]).singleOrDefault(); //1
+new LINQ([1,2]).singleOrDefault(); //exception
+new LINQ([]).singleOrDefault(); //null
+
+```
+
+first / firstOrDefault
+======================
+
+```
+new LINQ([1]).first(); //1
+new LINQ([1,2]).first(); //1
+new LINQ([]).first(); //exception
+new LINQ([1]).firstOrDefault(); //1
+new LINQ([1,2]).firstOrDefault(); //1
+new LINQ([]).firstOrDefault(); //null
+
+```
+
+last / lastOrDefault
+====================
+
+```
+new LINQ([1]).last(); //1
+new LINQ([1,2]).last(); //2
+new LINQ([]).last(); //exception
+new LINQ([1]).lastOrDefault(); //1
+new LINQ([1,2]).lastOrDefault(); //2
+new LINQ([]).lastOrDefault(); //null
+
+```
+
+groupBy
+=======
+
+Groups the elements of the array by a key.
+
+```
+var students = [{"id":1, "name":"Harding", "age":18},{"id":2, "name":"Johnson", "age":17},{"id":3, "name":"Farmer", "age":20},{"id":4, "name":"Owens", "age":17}];
+
+new LINQ(students)
+      .groupBy("s=>s.age")
+      .toDictionary();
+      //{"17":[{"id":2,"name":"Johnson","age":17},{"id":4,"name":"Owens","age":17}],"18":[{"id":1,"name":"Harding","age":18}],"20":[{"id":3,"name":"Farmer","age":20}]}
+
+      new LINQ(students)
+            .groupBy("s=>s.age","s.name")
+            .toDictionary();
+            //{{"17":["Johnson","Owens"],"18":["Harding"],"20":["Farmer"]}
+```
+
+orderBy
+=======
+
+Order the array ASC according to a specific key
+
+```
+var students = [{"id":1, "name":"Harding", "age":18},{"id":2, "name":"Johnson", "age":17},{"id":3, "name":"Farmer", "age":20},{"id":4, "name":"Owens", "age":17}];
+
+new LINQ(students)
+      .orderBy("s=>s.age")
+      .toArray(); //[{"id":2,"name":"Johnson","age":17},{"id":4,"name":"Owens","age":17},{"id":1,"name":"Harding","age":18},{"id":3,"name":"Farmer","age":20}]
+```
+
+orderByDescending
+=================
+
+Order the array DESC according to a specific key
+
+```
+var students = [{"id":1, "name":"Harding", "age":18},{"id":2, "name":"Johnson", "age":17},{"id":3, "name":"Farmer", "age":20},{"id":4, "name":"Owens", "age":17}];
+
+new LINQ(students)
+      .orderByDescending("s=>s.age")
+      .toArray(); //[{"id":3,"name":"Farmer","age":20},{"id":1,"name":"Harding","age":18},{"id":4,"name":"Owens","age":17},{"id":2,"name":"Johnson","age":17}]
+```
+
+distinct
+========
+
+Returns distinct elements from an array.
+
+```
+new LINQ([1,1,2,1,4,5,2,3,4,5])
+      .distinct()
+      .toArray(); //[1,2,4,5,3]
+```
+
+intersect
+=========
+
+Produces the set intersection of two sequences .
+
+```
+new LINQ([1,2,3,4,5,6])
+      .intersect([2,4,6,8])
+      .toArray(); //[2,4,6]
+```
+
+join
+====
+
+Correlates the elements of two sequences based on matching keys. [MSDN](https://msdn.microsoft.com/en-us/library/system.linq.enumerable.join.aspx)
+
+First parameter : first array selector
+
+Second parameter : second array selector
+
+Third parameter : result selector
+
+```
+var students = [{"id":1, "name":"Harding", "age":18,"class":1},{"id":2, "name":"Johnson", "age":17,"class":2},{"id":3, "name":"Farmer", "age":20,"class":2},{"id":4, "name":"Owens", "age":17,"class":3}];
+var classes = [{"number":1,"teacher":"Suzanne"},{"number":2,"teacher":"Adrian"},{"number":3,"teacher":"Brown"},{"number":4,"teacher":"White"}]
+
+new LINQ(students)
+      .join(classes,"s=>s.class","c=>c.number","s,c=>{name:s.name,teacher:c.teacher}")
+      .toArray(); //[{"name":"Harding","teacher":"Suzanne"},{"name":"Johnson","teacher":"Adrian"},{"name":"Farmer","teacher":"Adrian"},{"name":"Owens","teacher":"Brown"}]
+
+
+```
+
+toArray
+=======
+
+This function will evaluate all the queries you chained to the list and then it will return a javascript normal Array with the result
+
+```
+
+new LINQ([1,2,3,4])
+      .where("n=>n>2")
+      .toArray(); // [3,4]
+
+```
+
+\** This function should be used only when you chain queries that returns lists ,, or it will throw an exception
+
+toDictionary
+============
+
+This function will return a dictionary from the queries you request - If it was used with a function that returns a dictionary like [groupBy](#groupby) it will evaluate the queries and returns a dictionary
+
+```
+var points = [{"x":0,"y":0},{"x":0,"y":1},{"x":1,"y":0},{"x":1,"y":1}];
+
+new LINQ(points)
+      .groupBy("p=>p.x")
+      .toDictionary() //{0:[{"x":0,"y":0},{"x":0,"y":1}],1:[{"x":1,"y":0},{"x":1,"y":1}]}
+
+```
+
+-	You can provide a keySelectorFunction and/or a valueSelectorFunction as params to this functions
+
+```
+var points = [{"x":0,"y":0},{"x":0,"y":1},{"x":1,"y":0},{"x":1,"y":1}];
+
+new LINQ(points)
+    .groupBy("p=>p.x") // {0:[{"x":0,"y":0},{"x":0,"y":1}],1:[{"x":1,"y":0},{"x":1,"y":1}]}
+    .toDictionary("x=>x","p=>p.y"); // {0:[0,1],1:[0,1]}
+
+```
+
+-	If the result of evaluating the queries was an array, you need to provide a keySelectorFunction and a valueSelectorFunction
+
+```
+var students = [{"id":1, "name":"Harding", "age":18},{"id":2, "name":"Johnson", "age":17},{"id":3, "name":"Farmer", "age":20},{"id":4, "name":"Owens", "age":13}];
+
+new LINQ(points)
+      .where("s=>s.age >= 18")
+      .toDictionary("s=>s.id","s=>s.name") //{1:"Harding",3:"Farmer"}
+
+```
---
+++ src/linqer.js
@@ -0,0 +1,596 @@
+(function(root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    // AMD
+    define([], factory);
+  } else if (typeof exports === 'object') {
+    // Node, CommonJS-like
+    module.exports = factory();
+  } else {
+    // Browser globals (root is window)
+    root.LINQ = factory();
+  }
+})(this, function() {
+  function LINQ(array) {
+    //CTOR and check for valid data
+    if (!array || !(array instanceof Array))
+      throw new Error("You need to provide an array");
+
+    this._list = array;
+    this._queriesQueue = [];
+
+    this._getArray = _getArray;
+    this._setArray = _setArray;
+
+    this._enqueueExpression = _enqueueExpression;
+    this._evaluateExpressions = _evaluateExpressions;
+
+    //evaluators
+    this._eWhere = eWhere;
+    this._eSelect = eSelect;
+    this._eSkip = eSkip;
+    this._eTake = eTake;
+    this._eSkipTake = eSkipTake;
+    this._eReverse = eReverse;
+    this._eRemoveAll = eRemoveAll;
+    this._eGroupBy = eGroupBy;
+    this._eOrderBy = eOrderBy;
+    this._eDistinct = eDistinct;
+    this._eIntersect = eIntersect;
+    this._eJoin = eJoin;
+    this._eSelectMany = eSelectMany;
+
+    //API
+    this.toArray = toArray;
+    this.toDictionary = toDictionary;
+    this.where = where;
+    this.select = select;
+    this.count = count;
+    this.any = any;
+    this.all = all;
+    this.sum = sum;
+    this.average = average;
+    this.skip = skip;
+    this.take = take;
+    this.max = max;
+    this.min = min;
+    this.reverse = reverse;
+    this.removeAll = removeAll;
+    this.single = single;
+    this.singleOrDefault = singleOrDefault;
+    this.first = first;
+    this.firstOrDefault = firstOrDefault;
+    this.last = last;
+    this.lastOrDefault = lastOrDefault;
+    this.groupBy = groupBy;
+    this.orderBy = orderBy;
+    this.orderByDescending = orderByDescending;
+    this.distinct = distinct;
+    this.intersect = intersect;
+    this.join = join;
+    this.selectMany = selectMany;
+
+    return this;
+  }
+
+  //private
+  function _getArray() {
+    return this._list;
+  }
+
+  function _setArray(array) {
+    this._list = array;
+  }
+
+  //API
+  function toArray() {
+    this._evaluateExpressions();
+    if (!Array.isArray(this._list)) {
+      this._list = dictionaryToArray(this._list);
+    }
+    return this._list;
+  }
+
+  function toDictionary(keyFn, valueFn) {
+    this._evaluateExpressions();
+    if (Array.isArray(this._list)) {
+      if (!keyFn && !valueFn) {
+        throw new Exception("An array can't be converted to a dictionary without mapping functions");
+      } else {
+        keyFn = parse(keyFn);
+        valueFn = parse(valueFn);
+        this._list = arrayToDictionary(this._list, keyFn, valueFn);
+      }
+    } else if (keyFn || valueFn) {
+      keyFn = parse(keyFn || "x=>x");
+      valueFn = parse(valueFn || "x=>x");
+      this._list = mapDictionary(this._list, keyFn, valueFn)
+    }
+    return this._list;
+  }
+
+  function where(fn) {
+    this._enqueueExpression("where", fn);
+    return this;
+  }
+
+  function groupBy(keyFn, valueFn) {
+    this._enqueueExpression("groupBy", [keyFn, valueFn || "x=>x"]);
+    return this;
+  }
+
+  function skip(number) {
+    checkIfNumber(number, "Skip");
+    this._enqueueExpression("skip", number);
+    return this;
+  }
+
+  function take(number) {
+    checkIfNumber(number, "Take");
+    this._enqueueExpression("take", number);
+    return this;
+  }
+
+  function select(fn) {
+    this._enqueueExpression("select", fn);
+    return this;
+  }
+
+
+  function reverse() {
+    this._enqueueExpression("reverse");
+    return this;
+  }
+
+  function removeAll(fn) {
+    this._enqueueExpression("removeAll", fn);
+    return this;
+  }
+
+  function orderBy(fn) {
+    this._enqueueExpression("orderBy", fn);
+    return this;
+  }
+
+  function orderByDescending(fn) {
+    this.orderBy(fn).reverse();
+    return this;
+  }
+
+  function distinct() {
+    this._enqueueExpression("distinct");
+    return this;
+  }
+
+  function join(arr, key1Fn, key2Fn, resultFn) {
+    this._enqueueExpression("join", [arr, key1Fn, key2Fn, resultFn]);
+    return this;
+  }
+
+  function intersect(array) {
+    this.distinct();
+    this._enqueueExpression("intersect", array)
+    return this;
+  }
+
+  function selectMany(selectFn, resultFn) {
+    this._enqueueExpression("selectMany", [selectFn, resultFn]);
+    return this;
+  }
+
+  function count(fn) {
+    checkIflist(this._getArray(), "Count");
+    if (fn) {
+      this.where(fn);
+    }
+    return this.toArray().length;
+  }
+
+  function max(fn) {
+    checkIflist(this._getArray(), "Max");
+    if (fn) {
+      this.select(fn);
+    }
+    return Math.max.apply(null, this.toArray());
+  }
+
+  function min(fn) {
+    checkIflist(this._getArray(), "Min");
+    if (fn) {
+      this.select(fn);
+    }
+    return Math.min.apply(null, this.toArray());
+  }
+
+  function any(fn) {
+    checkIflist(this._getArray(), "Any");
+    return this.toArray().some(parse(fn));
+  }
+
+  function all(fn) {
+    checkIflist(this._getArray(), "All");
+    return this.toArray().every(parse(fn));
+  }
+
+  function sum(fn) {
+    checkIflist(this._getArray(), "Sum");
+    if (fn) {
+      this.select(fn);
+    }
+    return this.toArray().reduce(function(c, l) {
+      return c + l
+    }, 0);
+  }
+
+  function average(fn) {
+    checkIflist(this._getArray(), "Average");
+    if (fn) {
+      this.select(fn);
+    }
+    var a = this.toArray();
+    return a.reduce(function(c, l) {
+      return c + l
+    }, 0) / a.length;
+  }
+
+  function single(fn) {
+    checkIflist(this._getArray(), "Single");
+    if (fn) {
+      this.where(fn);
+    }
+    var a = this.toArray();
+    if (a.length > 1) {
+      throw new Error("Sequene contains " + a.length + " elements")
+    }
+    if (a.length === 0) {
+      throw new Error("Sequene doesn't contain any elements")
+    }
+    return a[0];
+  }
+
+  function singleOrDefault(fn) {
+    checkIflist(this._getArray(), "SingleOrDefault");
+    if (fn) {
+      this.where(fn);
+    }
+    var a = this.toArray();
+    if (a.length > 1) {
+      throw new Error("Sequene contains " + a.length + " elements")
+    }
+    if (a.length === 0) {
+      return null;
+    }
+    return a[0];
+  }
+
+  function first(fn) {
+    checkIflist(this._getArray(), "First");
+    if (fn) {
+      this.where(fn);
+    }
+    var a = this.toArray();
+    if (a.length === 0) {
+      throw new Error("Sequence doesn't contain any elements")
+    }
+    return a[0];
+  }
+
+  function firstOrDefault(fn) {
+    checkIflist(this._getArray(), "FirstOrDefault");
+    if (fn) {
+      this.where(fn);
+    }
+    var a = this.toArray();
+    if (a.length === 0) {
+      return null;
+    }
+    return a[0];
+  }
+
+  function last(fn) {
+    checkIflist(this._getArray(), "Last");
+    if (fn) {
+      this.where(fn);
+    }
+    var a = this.toArray();
+    if (a.length === 0) {
+      throw new Error("Sequence doesn't contain any elements")
+    }
+    return a[a.length - 1];
+  }
+
+  function lastOrDefault(fn) {
+    checkIflist(this._getArray(), "LastOrDefault");
+    if (fn) {
+      this.where(fn);
+    }
+    var a = this.toArray();
+    if (a.length === 0) {
+      return null;
+    }
+    return a[a.length - 1];
+  }
+
+  //evaluators
+
+  function eWhere(fn) {
+    checkIflist(this._getArray(), "Where");
+    this._setArray(this._getArray().filter(parse(fn)));
+  }
+
+  function eSelect(fn) {
+    checkIflist(this._getArray(), "Select");
+    this._setArray(this._getArray().map(parse(fn)));
+  }
+
+  function eSkip(number) {
+    checkIflist(this._getArray(), "Skip");
+    this._setArray(this._getArray().slice(number));
+  }
+
+  function eTake(number) {
+    checkIflist(this._getArray(), "Take");
+    this._setArray(this._getArray().slice(0, number));
+  }
+
+  function eSkipTake(numbers) {
+    checkIflist(this._getArray(), "SkipTake");
+    this._setArray(this._getArray().slice(numbers[0], numbers[0] + numbers[1]));
+  }
+
+  function eReverse() {
+    checkIflist(this._getArray(), "Reverse");
+    this._setArray(this._getArray().reverse());
+  }
+
+  function eRemoveAll(fn) {
+    checkIflist(this._getArray(), "RemoveAll");
+    fn = parse(fn);
+    this._setArray(this._getArray().filter(function(d) {
+      return !fn(d);
+    }));
+  }
+
+  function eGroupBy(mappingFunctions) {
+    checkIflist(this._getArray(), "GroupBy");
+    keyFn = parse(mappingFunctions[0]);
+    valueFn = parse(mappingFunctions[1]);
+    this._setArray(this._getArray().reduce(function(c, l) {
+      var key = keyFn(l);
+      var value = valueFn(l);
+      if (c[key]) {
+        c[key].push(value);
+      } else {
+        c[key] = [value];
+      }
+      return c;
+    }, {}));
+  }
+
+  function eOrderBy(fn) {
+    checkIflist(this._getArray(), "OrderBy");
+    fn = parse(fn);
+    this._setArray(this._getArray().sort(sortingFn(fn)));
+  }
+
+  function eJoin(params) {
+    checkIflist(this._getArray(), "Join");
+    checkIflist(params[0], "Join");
+    arr = params[0];
+    key1Fn = parse(params[1]);
+    key2Fn = parse(params[2]);
+    resultFn = parse(params[3]);
+    this._setArray(this._getArray().reduce(function(c, l) {
+      var matchedElements = arr.filter(function(e) {
+        return isEqual(key1Fn(l), key2Fn(e));
+      });
+      var joinedElements = matchedElements.map(function(e) {
+        return resultFn(l, e);
+      });
+      c = c.concat(joinedElements);
+      return c;
+    }, []));
+  }
+
+  function eIntersect(array) {
+    checkIflist(this._getArray(), "Intersect");
+    checkIflist(array, "Intersect");
+    if (array.length == 0) {
+      this._setArray([]);
+      return;
+    }
+    this._setArray(this._getArray().reduce(function(c, l) {
+      if (indexOf(array, l) > -1) {
+        c.push(l);
+      }
+      return c;
+    }, []));
+  }
+
+  function eDistinct() {
+    checkIflist(this._getArray(), "Distinct");
+    var arr = this._getArray();
+    this._setArray(arr.reduce(function(c, l, i) {
+      if (indexOf(arr, l) === i) {
+        c.push(l);
+      };
+      return c;
+    }, []));
+  }
+
+  function eSelectMany(params) {
+    checkIflist(this._getArray(), "SelectMany");
+    var selectFn = parse(params[0]);
+    var resultFn = params[1] ? parse(params[1]) : function(p, c) {
+      return c;
+    };
+    var arr = this._getArray();
+    //select items
+    arr = arr.map(function(l, i) {
+      var pc = [l, selectFn(l, i)]
+      return pc[1].map(function(o) {
+        return resultFn(pc[0], o);
+      });
+    });
+    //flatten items
+    var result = arr.reduce(function(c, l) {
+      c = c.concat(l);
+      return c;
+    }, []);
+    this._setArray(result);
+  }
+
+  //helpers
+  function indexOf(arr, obj) {
+    for (i in arr) {
+      if (typeof obj === "object" ? isEqual(arr[i], obj) : arr[i] == obj) {
+        return parseInt(i);
+      }
+    }
+    return -1;
+  }
+
+  function isEqual(a, b) {
+    //implement in case of arrays
+    ////optimize by length first
+    if (a === b) return true;
+    if (a == b) return true;
+    if (typeof a !== typeof b) return false;
+    if (typeof a !== "object")
+      return a == b;
+    if (Array.isArray(a)) {
+      if (Array.isArray(b)) {
+        if (a.length !== b.length) return false;
+        return (a.every(function(e) {
+          return indexOf(b, e) > -1;
+        }) && b.every(function(e) {
+          return indexOf(a, e) > -1;
+        }));
+      }
+      return false;
+    }
+    aProps = Object.keys(a);
+    bProps = Object.keys(b);
+    if (aProps.length !== bProps.length) return false;
+    return (aProps.every(function(k) {
+      return (typeof a[k] === 'object') ? isEqual(a[k], b[k]) : isEqual(a[k], b[k]);
+    }) && bProps.every(function(k) {
+      return (typeof b[k] === 'object') ? isEqual(b[k], a[k]) : isEqual(b[k], a[k]);
+    }));
+  }
+
+  function sortingFn(fn) {
+    return function(a, b) {
+      a = fn(a);
+      b = fn(b);
+      if (a < b) return -1;
+      if (a > b) return 1;
+      return 0;
+    };
+  }
+
+  function _enqueueExpression(eFn, param) {
+    var queue = this._queriesQueue;
+    if (queue.length >= 1) {
+      var lastItemIndex = queue.length - 1;
+      var lastQuery = queue[lastItemIndex];
+      //combine skip-take queries
+      if (eFn === "take" && lastQuery[0] === "skip") {
+        queue[lastItemIndex] = ["skipTake", [lastQuery[1], param]];
+      }
+    }
+
+    queue.push([eFn, param]);
+  }
+
+  function _evaluateExpressions() {
+    while (this._queriesQueue.length > 0) {
+      var query = this._queriesQueue.shift();
+      var eFn = query[0];
+      var fn = query[1];
+
+      eFn = eFn.charAt(0).toUpperCase() + eFn.slice(1)
+      eFn = "_e" + eFn;
+      if (this[eFn]) {
+        this[eFn](fn);
+      } else {
+        throw new Error("Unhandled query !")
+      }
+    }
+  }
+
+  function parse(query) {
+    var lambdaRegex = /w*\s*=>\s*w*/
+    if (typeof query !== "string" && !query) {
+      throw new Error("Not a valid fuction or lambda expression");
+    }
+    if (isFunction(query)) {
+      return query;
+    }
+    if (typeof query === "string" && lambdaRegex.test(query)) {
+      return lambdaToFunction(query)
+    }
+    throw new Error("Not a valid fuction or lambda expression");
+  }
+
+  function lambdaToFunction(expression) {
+    var tokens = expression.split("=>");
+    if (tokens.length > 2) {
+      throw new Error("LINQ.js doesn't support nested lambda yet!")
+    }
+    return (function() {
+      var fn = "temp = function(" + tokens[0].trim() + "){return " + tokens[1].trim() + "}";
+      return eval(fn)
+    })();
+  }
+
+  function isFunction(obj) {
+    //isFunction from underscore.js
+    return !!(obj && obj.constructor && obj.call && obj.apply);
+  }
+
+  function checkIfNumber(number, fName) {
+    if (typeof number !== "number") {
+      throw new Error(fName + " expects a number !")
+    }
+    return true;
+  }
+
+  function checkIflist(array, fName) {
+    if (!Array.isArray(array)) {
+      throw new Error(fName + " expects an array !")
+    }
+    return true;
+  }
+
+  function dictionaryToArray(dic) {
+    keys = Object.keys(dic);
+    return keys.map(function(key) {
+      return [key, dic[key]];
+    });
+  }
+
+  function mapDictionary(dict, keyFn, valueFn) {
+    return Object.keys(dict).reduce(function(c, l) {
+      c[keyFn(l)] = valueFn(dict[l]);
+      return c;
+    }, {})
+  }
+
+  function arrayToDictionary(array, keyFn, valueFn) {
+    return array.reduce(function(c, l) {
+      c[keyFn(l)] = valueFn(l);
+      return c;
+    }, {});
+  }
+
+  function ctor(array) {
+    return new LINQ(array);
+  }
+
+  // add toLinq in Array.prototype
+  Array.prototype.toLINQ = function() {
+      return new LINQ(this);
+    }
+    //Exports functions
+  return ctor;
+});
